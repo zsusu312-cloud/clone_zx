3a590ae3b2309dd9c537b8977744743c3bad23ec
zsusu312-cloud <zsusu312@gmail.com>
2025-11-21 11:33:52 +0000

    1

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
-<<<<<<< HEAD
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/",methods = ["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
-=======
->>>>>>> 843ddee7f87919cf66abe8087f2ebb639d2968b4
